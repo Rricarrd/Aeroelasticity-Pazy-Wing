e4fb6cd4e29202b9d59c0357c520a284e0644cbe
MarcTorrento <marc.torrento.vila@estudiantat.upc.edu>
2024-12-13 12:23:17 +0100

    update

--- PazyWing.m
+++ PazyWing.m
@@ -9,6 +9,8 @@ end
 p.EI = EI;
 p.GJ = GJ;
 p.xs = xs;
+p.rhoAl = 2795; % [kg/m^3] Density of Aluminium
+p.rhoNyl = 930; % [kg/m^3] Density of Nylon 12
 
 
 % M Matrix parameters
@@ -19,13 +21,14 @@ p.d = 1;
 
 %% Numerical discretization
 % Parameters
-p.n = 14;         % Panel node number
-p.Rs = 38.4;       % Rib spacing
-p.Lw = 550;        % Wing lenght
+p.n = 14;              % Panel node number
+p.Rs = 0.0384;         % [m] Rib spacing
+p.Lw = 0.550;          % [m] Wing lenght
 p.Ls = p.Lw - 13*p.Rs; % Last wing segment space
-p.nj = 3;          % DOFs per node (theta, eta i gamma)
-p.ng = 1;          % Gauss points per element
-p.en = 2;          % Element nodes
+p.nj = 3;              % DOFs per node (theta, eta i gamma)
+p.ng = 1;              % Gauss points per element
+p.en = 2;              % Element nodes
+p.Rt = 0.004;          % [m] Rib thickness
  
 
 % Discretization and conectivities
---
+++ computeMasses.m
@@ -0,0 +1,21 @@
+function [mu mrib] = computeMasses(p)
+
+airfoil = importdata('NACA0018.txt'); % Geometry of the airfoil
+
+x = flip(airfoil(:,1))/10; % [m]
+y = flip(airfoil(:,2))/10; % [m]
+
+A1 = double(trapz(x(1:5),y(1:5)))*2; % [m^2] Surface of the first plastic part of the rib
+mu1 = A1*p.rhoNyl;
+
+A2 = double(trapz(x(5:17),y(5:17)))*2 - 0.005*0.03 - 0.06*0.00225; % [m^2] Surface of the second plastic part of the rib
+mrib = A2*p.rhoNyl*p.Rt; % [kg] Mass of the rib
+
+A3 = double(trapz(x(17:18),y(17:18)))*2; % [m^2] Surface of the third plastic part of the rib
+mu3 = A3*p.rhoNyl;
+
+A4 = 0.06*0.00225; % [m^2] Surface of the Aluminium bar
+mu4 = A4*p.rhoAl;
+
+mu = mu1 + mu3 + mu4; % [kg/m] Linear beam density
+
